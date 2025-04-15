2896206b9e17ad9d953d50bf5af875b75eb2a909
Leonardo Balbino <leovinicius42@outlook.com>
2025-04-15 20:34:06 -0300

    fix: ajuste correcao cors

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ import mainRouter from './src/index.js';
 const app = express();
 const PORT = process.env.PORT || 9000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/api', mainRouter);
